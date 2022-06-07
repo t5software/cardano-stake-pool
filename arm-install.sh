bcc0fc69ac97143393f2273e05700ea3535142ae
t5software <58944328+t5software@users.noreply.github.com>
2022-06-06 17:16:20 -0700

    fix some interaction lines #1

--- arm-install.sh
+++ arm-install.sh
@@ -4,17 +4,17 @@ exec bash
 mkdir "$HOME/tmp";cd "$HOME/tmp"
 curl -sS -o prereqs.sh https://raw.githubusercontent.com/cardano-community/guild-operators/master/scripts/cnode-helper-scripts/prereqs.sh
 chmod 755 prereqs.sh
-./prereqs.sh
+# the line below still needs user interaction
+NEEDRESTART_SUSPEND=true ./prereqs.sh
 exec bash
 
 # extra arm steps
-sudo apt install llvm-13
-sudo apt install clang-13 libnuma-dev
+NEEDRESTART_SUSPEND=true sudo apt install -y llvm-13
+NEEDRESTART_SUSPEND=true sudo apt install -y clang-13 libnuma-dev
 sudo ln -s /usr/bin/llvm-config-13 /usr/bin/llvm-config
 sudo ln -s /usr/bin/opt-13 /usr/bin/opt
 sudo ln -s /usr/bin/llc-13 /usr/bin/llc
 sudo ln -s /usr/bin/clang-13 /usr/bin/clang
-exec bash
 
 # clone cardano-node
 cd ~/git
@@ -28,8 +28,8 @@ git pull
 git checkout $(curl -s https://api.github.com/repos/input-output-hk/cardano-node/releases/latest | jq -r .tag_name)
 
 echo BUILD-ALL
+exec bash
 $CNODE_HOME/scripts/cabal-build-all.sh
-
 exec bash
 
 cardano-cli version
